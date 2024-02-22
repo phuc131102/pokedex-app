091fcaa61f722cb977a2ed83deaeeb9fcc6f7b71
phuc131102 <phuc32969@gmail.com>
2024-02-22 10:36:55 +0700

    update

--- src/components/Type/Type.jsx
+++ src/components/Type/Type.jsx
@@ -6,6 +6,7 @@ import Electric from "../../assets/type/Electric.png";
 import Fairy from "../../assets/type/Fairy.png";
 import Dragon from "../../assets/type/Dragon.png";
 import Fighting from "../../assets/type/Fighting.png";
+import Flying from "../../assets/type/Flying.png";
 import Poison from "../../assets/type/Poison.png";
 
 const getImageSource = (type) => {
